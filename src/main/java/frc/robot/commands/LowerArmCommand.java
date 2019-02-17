6b26068c10f29271c7dd88574d365e2d031b11c0
Frankie Lucco <francesrlucco@gmail.com>
2019-02-17 13:57:37 -0800

    lower hatch command

--- src/main/java/frc/robot/commands/LowerArmCommand.java
+++ src/main/java/frc/robot/commands/LowerArmCommand.java
@@ -8,12 +8,17 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.subsystems.HatchArm;
 
-public class PlaceHatchCommand extends Command {
-  public PlaceHatchCommand() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
+
+public class LowerArmCommand extends Command {
+  public LowerArmCommand() {
+    requires(Robot.hatchArm);
   }
+
+  private final double kArmLowerMax = 0.05;
+	private final double kArmLowStall = 0.00;
 
   // Called just before this Command runs the first time
   @Override
@@ -23,12 +28,25 @@ public class PlaceHatchCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    double angle = Robot.hatchArm.getAngle();
+		double out = 0;
+		if (angle > (5 + HatchArm.startingAngle)) {
+			out = Math.cos(Math.toRadians(angle))* kArmLowerMax;
+		}
+		if (angle < Math.toRadians(-30))
+			out *= 3;
+		else if (angle > Math.toRadians(80))
+			out -= 0.2;
+		else
+			out -= 0.1;
+		Robot.hatchArm.set(out);
+
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return Robot.hatchArm.getAngle() < (5 + HatchArm.startingAngle);
   }
 
   // Called once after isFinished returns true
